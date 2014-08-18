70d2f2f0d3786b33b7237c087d7830b7dd361aed
Infizerr <infizerr@zombiebladesystems.fake>
2014-08-18 15:59:57 +0200

    Initial Commit

---
+++ LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using PBKDF2_Generator_WP.Resources;
+
+namespace PBKDF2_Generator_WP
+{
+    /// <summary>
+    /// Bietet Zugriff auf Zeichenfolgenressourcen.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using PBKDF2_Generator_WP.Resources;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace PBKDF2_Generator_WP
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Konstruktor
+        public MainPage()
+        {
+            InitializeComponent();
+
+            // Beispielcode zur Lokalisierung der ApplicationBar
+            //BuildLocalizedApplicationBar();
+        }
+
+        private void genbtn_Click(object sender, RoutedEventArgs e)
+        {
+            string use = svctb.Text;
+            while (use.Length < 8)
+            {
+                use += "O";
+            }
+            Rfc2898DeriveBytes rfc = new Rfc2898DeriveBytes(keytb.Text, Encoding.UTF8.GetBytes(use.ToLower()), 4096);
+            String res2898 = Convert.ToBase64String(rfc.GetBytes(6)).Replace("=", "");
+            MessageBoxResult res = MessageBox.Show(res2898, "Result-Zum Speichern in Zwischenablage OK", MessageBoxButton.OKCancel);
+            if (res == MessageBoxResult.OK)
+            {
+                System.Windows.Clipboard.SetText(res2898);
+            }
+        }
+
+        // Beispielcode zur Erstellung einer lokalisierten ApplicationBar
+        //private void BuildLocalizedApplicationBar()
+        //{
+        //    // ApplicationBar der Seite einer neuen Instanz von ApplicationBar zuweisen
+        //    ApplicationBar = new ApplicationBar();
+
+        //    // Eine neue Schaltfläche erstellen und als Text die lokalisierte Zeichenfolge aus AppResources zuweisen.
+        //    ApplicationBarIconButton appBarButton = new ApplicationBarIconButton(new Uri("/Assets/AppBar/appbar.add.rest.png", UriKind.Relative));
+        //    appBarButton.Text = AppResources.AppBarButtonText;
+        //    ApplicationBar.Buttons.Add(appBarButton);
+
+        //    // Ein neues Menüelement mit der lokalisierten Zeichenfolge aus AppResources erstellen
+        //    ApplicationBarMenuItem appBarMenuItem = new ApplicationBarMenuItem(AppResources.AppBarMenuItemText);
+        //    ApplicationBar.MenuItems.Add(appBarMenuItem);
+        //}
+    }
+}
